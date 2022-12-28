d4af8e0a113f8e11aac9e19df7930432a8d5f703
Hossein Zolfi <hossein.zolfi@gmail.com>
2022-12-28 10:23:42 +0330

    Format autocmds.lua file.

--- autocmds.lua
+++ autocmds.lua
@@ -3,26 +3,26 @@ local autocmd = vim.api.nvim_create_autocmd
 local opt = vim.opt
 
 autocmd("FileType", {
-   pattern = "norg",
-   callback = function()
-      -- vim.opt.laststatus = 0
-      opt.number = false
-      opt.showtabline = 0
-      opt.cole = 1
-      opt.foldlevel = 10
-   end,
+  pattern = "norg",
+  callback = function()
+    -- vim.opt.laststatus = 0
+    opt.number = false
+    opt.showtabline = 0
+    opt.cole = 1
+    opt.foldlevel = 10
+  end,
 })
 
 autocmd("BufReadPost", {
   pattern = "*.*",
-  callback = function ()
-    local line, col = unpack(vim.api.nvim_buf_get_mark(0, "\""))
-    local lastline = vim.fn.line("$")
+  callback = function()
+    local line, col = unpack(vim.api.nvim_buf_get_mark(0, '"'))
+    local lastline = vim.fn.line "$"
     if line > lastline then
-        line = lastline
+      line = lastline
     end
-    if {line, col} ~= {0, 0} then
-      vim.api.nvim_win_set_cursor(0, {line, col})
+    if { line, col } ~= { 0, 0 } then
+      vim.api.nvim_win_set_cursor(vim.api.nvim_get_current_win(), { line, col })
     end
   end,
 })
